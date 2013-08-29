e799cc1adfcc0e995b3c19e0fe3db4fbc3a71216
Caleb Cauthon <calebcauthon@gmail.com>
2013-08-29 00:20:25 -0500

    add players from the front end interface

--- web.rb
+++ web.rb
@@ -26,7 +26,7 @@ get '/log-action' do
       params.delete('action_number');
       params.delete('location')
       url = "https://lusl-dev.firebaseio.com/game_logs/#{location}/logs.json"
-      puts url
+      
       c = Curl::Easy.new(url)
       c.http_post(params.to_json)
       
@@ -39,7 +39,7 @@ get '/log-action' do
       params.delete('controller')
       params.delete('action_number');
       params.delete('location')
-      puts "test 123"
+      
       url = "https://lusl-dev.firebaseio.com/game_logs/#{location}.json"
 
       c = Curl.patch(url, params.to_json);
